493cb81f22ff6d16d569897a1be03d8912d2c4f8
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2015-12-26 14:43:54 +0330

    white, message colors, login with underlying salt check

--- js/fn.js
+++ js/fn.js
@@ -3,11 +3,11 @@ var a  = (function () {
 var htmls = {
 		ajaxImg: ''
 		+	'<img '
-		+	'src="images/ajax-loader (abi).gif" '
+		+	'src="images/ajax-loader.gif" ' // ajax-loader-728.gif
 		+	'style="'
 		+		'position: absolute;'
 		+		'top: 50%;'
-		+		'left: 11em;'
+		+		'left: 11em;'// left: 43%;
 		+	'"'
 		+	' />'
 	},
@@ -36,30 +36,31 @@ var htmls = {
 		}
 	},
 	messages = {
-		undefinedUser:			'این نام کاربری در سایت وجود ندارد.'					,
-		wrongPass:				'رمز عبور اشتباه است.'							,
-		success:				'شما با موفقیت وارد سامانه شدید.'					,
-		during:					'در حال دریافت اطلاعات...'						,
-		heading:				'ورود به سامانه'								,
-		checkingUser:			'در حال بررسی نام کاربری.'						,
-		userCheckSuccess:		'نام کاربری تایید شد.'							,
-		userCheckFailed:		'خطا در بررسی نام کاربری، دوباره نام کاربری را بنویسید.',
-		userCheckPending:		'بررسی نام کاربری هنوز به اتمام نرسیده است.',
-		everythingOk:			'آماده برای ورود به سامانه'
+		undefinedUser:			'این نام کاربری در سایت وجود ندارد.'							,
+		wrongPass:				'رمز عبور اشتباه است.'								,
+		success:				'شما با موفقیت وارد سامانه شدید.'							,
+		during:					'در حال دریافت اطلاعات...'								,
+		heading:				'ورود به سامانه'									,
+		checkingUser:			'در حال بررسی نام کاربری.'								,
+		userCheckSuccess:		'نام کاربری تایید شد.'									,
+		userCheckFailed:		'خطا در ارتباط با سرور، دوباره نام کاربری را بنویسید.'					, // 'خطا در بررسی نام کاربری، دوباره نام کاربری را بنویسید.'
+		userCheckPending:		'بررسی نام کاربری هنوز به اتمام نرسیده است.'						,
+		everythingOk:			'آماده برای ورود به سامانه.'								,
+		saltReqFailed:			'خطا در ارتباط با سرور، دوباره امتحان کنید.'
 	},
 	colors = {
 		highlight:				'red'										, // FFCACA
-		success:				'rgba(127, 255, 0, 0.36)'					,
-		error:					'rgba(255, 0, 0, 0.36)'						,
+		success:				'rgba(127, 255, 0, 0.9)'					, // rgba(127, 255, 0, 0.36)
+		error:					'rgba(255, 0, 0, 0.9)'						, // rgba(255, 0, 0, 0.36)
 		usernameDefaultBg:		'rgba(255, 255, 255, 0.2)'					,
 		usernameDefaultColor:	'black'										,
 		usernameDisabledBg:		'rgba(74, 74, 74, 0.45)'					,
 		usernameDisabledColor:	'rgba(206, 206, 206, 0.52)'					,
 		usernameSuccessBg:		'rgba(169, 255, 0, 0.36)'					,
 		usernameSuccessColor:	'rgb(180, 255, 0)'							,
-		userCheck:				'rgba(0, 196, 255, 0.36)'					, // rgba(0, 0, 0, 0.36)
+		userCheck:				'rgb(150, 150, 150)'						, // rgba(0, 196, 255, 0.36)		rgba(0, 0, 0, 0.36)
 		userCheckSuccess:		'rgba(180, 255, 0, 0.36)'					,
-		userCheckFailed:		'rgba(255, 0, 0, 0.36)'			
+		userCheckFailed:		'rgb(150, 150, 150)'						  // rgba(255, 0, 0, 0.36)
 	},
 	dom = {
 		form:					$( selectors.form				)			,
@@ -82,11 +83,13 @@ var htmls = {
 	css = {
 		messagesDefault: {
 			top: '40%',
-			padding: '1em'
+			padding: '1em',
+			borderRadius: ''
 		},
 		messagesTop: {
 			top: '0',
-			padding: '0'
+			padding: '0',
+			borderRadius: '1em 1em 0px 0px'
 		},
 		usernameDefault: {
 			cursor: 'auto',
@@ -110,11 +113,6 @@ var htmls = {
 			transform: 'translate(10%, -100%) scale(0.70)'
 		}
 	},
-	flags = {
-		uRdy : false,
-		pRdy : false,
-		saltRdy : false
-	},
 	urls = {
 		auth: 'http://10.255.148.60/cgi-bin/oauth2/authenticate' //'http://185.4.29.188/cgi-bin/oauth2/authenticate'
 	},
@@ -122,14 +120,28 @@ var htmls = {
 		authenticate: 'authenticate',
 		getSalt: 'GetSalt'
 	},
-	salt = '',
+	flags = {
+		uRdy : false,
+		pRdy : false,
+		saltRdy : false,
+		saltReqSuccess: undefined,
+		formSubmitted: false
+	},
 	constants = {
-		ANI_DUR: 800,
+		ANI_DUR_BASE: 800,
+		ANI_DUR_VERYFAST: 100,
+		ANI_DUR_FAST: 200,
+		ANI_DUR_MEDIUM: 1000,
+		ANI_DUR_SLOW: 2000,
+		ANI_CLASSES_USED: 'animated shake tada flash',
+		ANI_END_EVENTS: 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
 		USER_MIN_CHAR: 3,
 		PASS_MIN_CHAR: 1,
-		ANI_CLASSES_USED: 'animated shake tada flash',
-		ANI_END_EVENT: 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
-		SALT_WAIT_TIME: 2000
+		SALT_WAIT_TIME: 2000,
+		FLAG_CHECK_INTERVAL: 1000
+	},
+	data = {
+		salt: ''
 	};
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 function isObject (a) {
@@ -147,17 +159,36 @@ function aniCssEnd () {
 	$(this).removeClass( constants.ANI_CLASSES_USED );
 }
 function inputAni(e) {
-	if ( a.dom.username.val().length !== 0 ) {
-		a.dom.userLabel.css( css.userLabelFocused );
+	if ( dom.username.val().length !== 0 ) {
+		dom.userLabel.css( css.userLabelFocused );
 	} else {
-		a.dom.userLabel.css( css.userLabelDefault );
+		dom.userLabel.css( css.userLabelDefault );
 	}
 }
+var ie = (function () {
+	return {
+		inputUserIe: function () {
+			if ( dom.userLabel.css('transform') === 'none' ) {
+				dom.userLabel.css('top', '-2em');
+			} else {
+				dom.userLabel.css('top', '0');
+			}
+			
+		},
+		inputPassIe: function  () {
+			if ( dom.passLabel.css('transform') === 'none' ) {
+				dom.passLabel.css('top', '-2em');
+			} else {
+				dom.passLabel.css('top', '0');
+			}
+		}
+	};
+}());
+
 	
 var validate = (function () {
 	var main = function () {
-		var form = dom.form,
-			submit = dom.submit
+		var submit = dom.submit,
 			username = dom.username,
 			password = dom.password,
 			uLength = username.val().trim().length,
@@ -167,53 +198,58 @@ var validate = (function () {
 			submit.attr({disabled: true});
 			setTimeout(function () {
 				submit.attr({disabled: false});
-			}, constants.ANI_DUR);
+			}, constants.ANI_DUR_BASE);
 		};
 		var effect = function (which) {
 			if ( typeof which === 'undefined' || isEmptyString(which) ) { throw new Error('effect:  Invalid argument.'); }
 			
-			dom[which].effect('highlight', {color: colors.highlight}, constants.ANI_DUR);
+			dom[which].effect('highlight', {color: colors.highlight}, constants.ANI_DUR_BASE);
 			$( selectors.shakePrefix + which ).toggleClass('animated shake');
 		};
 	
 		if ( uLength < constants.USER_MIN_CHAR ) {	// username not ready
 			effect('username');
 			disable();
-			a.flags.uRdy = false;
+			flags.uRdy = false;
 			username[0].focus();
 		} else {
-			a.flags.uRdy = true;
+			flags.uRdy = true;
 			if (!a.flags.pRdy) { password[0].focus(); }
 		}
 		
 		if ( pLength < constants.PASS_MIN_CHAR ) {	// password not ready
 			effect('password');
 			disable();
-			a.flags.pRdy = false;
+			flags.pRdy = false;
 			if (a.flags.uRdy) { password[0].focus(); } else { username[0].focus(); }
 		} else {
-			a.flags.pRdy = true;
+			flags.pRdy = true;
 			if (!a.flags.uRdy) { username[0].focus(); }
 		}
 		
+		/*
 		if ( a.flags.saltRdy === true ) {
 			a.flags.uRdy = true;
 			alertify.success(messages.everythingOk);
 		} else if ( a.flags.saltRdy === false ) {
 			if ( a.flags.uRdy === true ) {
 				dom.submit.attr( {disabled: true} );
-				username.effect('highlight', {color: colors.highlight}, constants.ANI_DUR, function () {
+				username.effect('highlight', {color: colors.highlight}, constants.ANI_DUR_BASE, function () {
 					dom.submit.attr( {disabled: false} );
 				});
 				alertify.error( messages.userCheckPending );
 				a.flags.uRdy = false;
 			}
 		}
+		*/
 	};
 	return main;
 }());
 
 var submit = (function () {
+	var breakSaltRdy = function () {
+		a.flags.saltRdy = false;
+	};
 	var getFormData = function () {
 		var data = '';
 		data += 'action=authent&';
@@ -221,31 +257,83 @@ var submit = (function () {
 		data += '&redirect_uri=';
 		data += dom.hiddenRedirectUri.val();
 		return data;
-	}
+	};
 	var beforeSend = function () {
-		a.dom.messages.animate( {opacity: '0'} );
-		a.dom.username.css( css.usernameDefault );
-		
+		dom.username.css( css.usernameDefault );
+		dom.messages.animate( {opacity: '0'}, constants.ANI_DUR_VERYFAST );
 		
 		dom.wrapper.addClass('form-success');
-		dom.title.animate({fontSize: '16px'});
+		dom.title.animate({fontSize: '16px'}, constants.ANI_DUR_BASE);
 		dom.title.text(messages.during);
 		dom.username.attr({disabled: true});
-		dom.password.attr({disabled: true});
+		//dom.password.attr({disabled: true});	if password is disabled "form.serialize" will not pick it up
 		dom.submit.attr({disabled: true});
 		dom.form.animate({opacity: 0}, function () {
 			dom.form.css({visibility: 'hidden'});
 		});
 		dom.card.prepend( $.parseHTML( htmls.ajaxImg ) );
-	}
-	var callback = function (success, data) {
-		if ( typeof success !== 'boolean' ) { throw new Erro('done:  Argument is not boolean.'); }
+	};
+	var undoBeforeSend = function () {
+		dom.wrapper.removeClass('form-success');
+		dom.title.animate({fontSize: '40px'}, constants.ANI_DUR_BASE);
+		dom.title.text(messages.heading);
+		$( selectors.later.ajaxImg ).remove();
+		dom.messages.css(css.messagesDefault);
+		dom.messages.empty();
+	};
+	var callback = function (success, data, saltReqStat) {
+		if ( typeof success !== 'boolean' ) { throw new Error('submit callback:  The first argument is not boolean.'); } else
+		if ( typeof saltReqStat !== 'boolean' ) { throw new Error('submit callback:  The third argument is not boolean.'); }
 		
 		if ( success === true ) {
+			undoBeforeSend();
+			dom.messages.css({background : colors.success});
+			dom.messages.append( $.parseHTML('<p>' + messages.success + '</p>') );
+			dom.messages.animate({opacity: '1'}, constants.ANI_DUR_VERYFAST, function () {
+				$( selectors.later.resultMessage ).toggleClass('animated tada');
+			});
+			
+			
+			setTimeout(function () {
+				dom.messages.animate({opacity: '0'}, constants.ANI_DUR_MEDIUM, function () {
+					//window.location.replace( data.url );
+					window.location.reload();
+				});
+			}, constants.ANI_DUR_SLOW);
 			
 		} else if ( success === false ) {
+			undoBeforeSend();
+			dom.messages.css({background : colors.error});
 			
+			var msg = ( saltReqStat === true ) ? messages.wrongPass : messages.saltReqFailed; // important
+			console.log(msg);
+			dom.messages.append( $.parseHTML('<p>' + msg + '</p>') );
+			dom.messages.animate({opacity: '1'}, constants.ANI_DUR_VERYFAST, function () {
+				$( selectors.later.resultMessage ).toggleClass('animated wobble');
+			});
+			
+			
+			setTimeout(function () {
+				dom.messages.animate({opacity: '0'}, constants.ANI_DUR_MEDIUM, function () {
+					dom.form.css({visibility: 'visible'});
+					dom.form[0].reset();
+					dom.form.animate({opacity: 1}, constants.ANI_DUR_MEDIUM, function () {
+						flags.formSubmitted = false;
+						
+						dom.username.attr({disabled: false});
+						dom.password.attr({disabled: false});
+						dom.submit.attr({disabled: false});
+						
+						dom.username[0].focus();
+					});
+				});
+				flags.saltRdy = false;
+			}, constants.ANI_DUR_SLOW);
 		}
+		
+		
+		
+		/* // previous way
 		dom.wrapper.removeClass('form-success');
 		dom.title.animate({fontSize: '40px'}, 400);
 		dom.title.text(messages.heading);
@@ -289,11 +377,38 @@ var submit = (function () {
 				flags.saltRdy = false;
 			}, 3000);
 		}
-	}
+		*/
+	};
+	var makeAjaxCall = function (saltStatus) {
+		dom.hiddenSalt.val( a.data.salt );
+		
+		$.ajax({
+			url: a.urls.auth,
+			type: 'GET',
+			dataType: 'json',
+			data: a.submit.getFormData()
+		})
+		.done(function (data) {
+			var success = false;
+			if ( data[0].status === 'success' )  {
+				success = true;
+				alertify.success(data[0].status);
+			}  else if ( data[0].status === 'error' ) {
+				success = false;
+				alertify.error(data[0].status + '<br />' + data[0].desc );
+			}
+			submit.callback( success, data[0], saltStatus );
+		})
+		.fail(function () {
+			submit.callback(false, undefined, saltStatus);
+		});
+	};
 	return {
+		breakSaltRdy: breakSaltRdy,
 		getFormData: getFormData,
 		beforeSend: beforeSend,
-		callback: callback
+		callback: callback,
+		makeAjaxCall: makeAjaxCall
 	};
 }());
 
@@ -301,69 +416,76 @@ var change = (function () {
 	var timer;
 	
 	var beforeSend = function () {
+		if ( flags.formSubmitted === true ) { return; }
+		dom.messages.css( css.messagesTop );
+		dom.messages.css( {opacity: '0'} );
 		alertify.log( a.messages.checkingUser );
-		a.dom.username.attr({disabled: true});
-		a.dom.username.css( css.usernameDisabled );
-		
-		a.dom.messages.animate( {opacity: '0'}, function () {
-			a.dom.messages.empty();
-			a.dom.messages.css( css.messagesTop );
-			a.dom.messages.css( {background : a.colors.userCheck} );
-			a.dom.messages.append( $.parseHTML('<p>' + a.messages.checkingUser + '</p>') );
-			a.dom.messages.animate( {opacity: '1'}, function () {
-				// var currentText = $(selectors.later.resultMessage).text(),
-					// counter = 0;
-				// timer = setInterval(function () {
-					// if (counter < 3) {
-						// $(selectors.later.resultMessage).text( currentText += '.' );
-					// } else if ( counter === 4 ) {
-						// $(selectors.later.resultMessage).text( currentText );
-					// }
-				// }, 500);
-			});
+		dom.username.attr({disabled: true});
+		//dom.username.css( css.usernameDisabled );
+
+		dom.messages.animate( {opacity: '0'}, constants.ANI_DUR_VERYFAST, function () {
+			dom.messages.empty();
+			dom.messages.css( {background : a.colors.userCheck} );
+			dom.messages.append( $.parseHTML('<p>' + a.messages.checkingUser + '</p>') );
+			dom.messages.animate( {opacity: '1'}, constants.ANI_DUR_VERYFAST );
 		});
 		
-	}
+		/* // ticking dots function
+		function () {
+			var currentText = $(selectors.later.resultMessage).text(),
+				counter = 0;
+			timer = setInterval(function () {
+				if (counter < 3) {
+					$(selectors.later.resultMessage).text( currentText += '.' );
+				} else if ( counter === 4 ) {
+					$(selectors.later.resultMessage).text( currentText );
+				}
+			}, 500);
+		}
+		*/
+	};
 	var callback = function (success, data) {
 		if ( success === true ) {
-			a.salt = data;
+			flags.saltRdy = true;
+			dom.messages.animate( {opacity: '0'} );
+			dom.username.attr({disabled: false});
+			/*
 			setTimeout(function () {
-				clearInterval(timer);
-				a.flags.saltRdy = true;
-				alertify.success( messages.userCheckSuccess );
-				
-				a.dom.messages.animate( {opacity: '0'}, function () {
-					a.dom.messages.css( {background : a.colors.userCheckSuccess} );
-					a.dom.messages.empty();
-					a.dom.messages.append( $.parseHTML('<p>' + a.messages.userCheckSuccess + '</p>') );
-					a.dom.messages.animate( {opacity: '1'}, function () {
-						a.dom.userLabel.css( css.userLabelDefault ); // important
-						a.dom.username.css( css.usernameSuccess );
-						a.dom.username.attr({disabled: false});
-					});
-				});
+				flags.saltRdy = true;
+				//alertify.success( messages.userCheckSuccess );
+				dom.messages.animate( {opacity: '0'} );
+				//dom.messages.animate( {opacity: '0'}, function () {
+				//	dom.messages.css( {background : a.colors.userCheckSuccess} );
+				//	dom.messages.empty();
+				//	dom.messages.append( $.parseHTML('<p>' + a.messages.userCheckSuccess + '</p>') );
+				//	dom.messages.animate( {opacity: '1'}, function () {
+				//		dom.userLabel.css( css.userLabelDefault ); // important
+						//dom.username.css( css.usernameSuccess );
+						dom.username.attr({disabled: false});
+				//	});
+				//});
 				
 			}, constants.SALT_WAIT_TIME );
+			*/
 			
 		} else if (success === false) {
-			clearInterval(timer);
+			flags.saltRdy = false;
 			alertify.error( messages.userCheckFailed );
 			
-			a.dom.messages.animate( {opacity: '0'}, function () {
-				a.dom.messages.css( {background : a.colors.userCheckFailed} );
-				a.dom.messages.empty();
-				a.dom.messages.append( $.parseHTML('<p>' + a.messages.userCheckFailed + '</p>') );
-				a.dom.messages.animate( {opacity: '1'}, function () {
-					a.dom.userLabel.css( css.userLabelDefault ); // important
-					a.dom.username.css( css.usernameDefault );
-					a.dom.username.attr({disabled: false});
-					a.dom.username.val('');
-					a.dom.username[0].focus();
+			dom.messages.animate( {opacity: '0'}, function () {
+				dom.messages.css( {background : a.colors.userCheckFailed} );
+				dom.messages.empty();
+				dom.messages.append( $.parseHTML('<p>' + a.messages.userCheckFailed + '</p>') );
+				dom.messages.animate( {opacity: '1'}, function () {
+					dom.userLabel.css( css.userLabelDefault ); // important
+					//dom.username.css( css.usernameDefault );
+					dom.username.attr({disabled: false});
+					dom.username.val('');
+					dom.username[0].focus();
 				});
 			});
 		}
-	}
-
+	};
 	return {
 		beforeSend: beforeSend,
 		callback: callback
@@ -378,7 +500,7 @@ return {
 	flags : flags,
 	urls: urls,
 	actions: actions,
-	salt: salt,
+	data: data,
 	constants: constants,
 	
 	validate: validate,
@@ -386,7 +508,8 @@ return {
 	change: change,
 	
 	aniCssEnd: aniCssEnd,
-	inputAni: inputAni
+	inputAni: inputAni,
+	ie: ie
 };
 
 
--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 $(function () {
-	a.dom.username.on('focus', a.inputAni);
-	a.dom.username.on('focusout', a.inputAni);
+	a.dom.shakeUser.on( a.constants.ANI_END_EVENTS, a.aniCssEnd);
+	a.dom.shakePass.on( a.constants.ANI_END_EVENTS, a.aniCssEnd);
+	a.dom.username.on('keyup keydown', a.submit.breakSaltRdy);
+	a.dom.username.on('focus focusout', a.inputAni);
+	a.dom.username.on('focus focusout', a.ie.inputUserIe);
+	a.dom.password.on('focus focusout', a.ie.inputPassIe);
 	
-	$( a.selectors.submit ).on('click', function (event) {
+	$( a.dom.submit ).on('click', function (event) {
 		if ( typeof $(this).attr('disabled') !== 'undefined' ) { return; }
 		if ( a.dom.shakeUser.is(':animated') || a.dom.shakeUser.is(':animated') ) { return; }
 		event.preventDefault();
 		
-		a.validate(); // validate will set some flags, we act base on those flags from this point forward.
+		a.validate();	// validate will set uRdy and pRdy flags.
 		
 		if (a.flags.uRdy && a.flags.pRdy) {
 			//form.submit();
-			a.dom.hiddenUsername	.val(	$( a.selectors.username ).val().trim()		);
-			a.dom.hiddenSalt		.val(	a.salt										);
-			a.dom.hiddenPassword	.val(	$( a.selectors.password ).val().trim()		);
+			a.flags.formSubmitted = true;
+			a.submit.beforeSend();
+			
+			if ( a.flags.saltRdy === true ) {
+				a.submit.makeAjaxCall(true);
+			} else if ( a.flags.saltRdy === false )  {
+				
+				var flagChecker = setInterval(function () {
+					if (a.flags.saltRdy === true) {
+						clearInterval(flagChecker);
+						a.submit.makeAjaxCall(true);
+						
+					} else if ( a.flags.saltReqSuccess === false ) {
+						clearInterval(flagChecker);
+						a.submit.makeAjaxCall(false);
+					}
+				}, a.constants.FLAG_CHECK_INTERVAL );
+			}
+			
+			//a.dom.hiddenUsername	.val(	$( a.selectors.username ).val().trim()		);
+			//a.dom.hiddenSalt		.val(	a.data.salt									);
+			//a.dom.hiddenPassword	.val(	$( a.selectors.password ).val().trim()		);
+			/*
 			$.ajax({
 				url: a.urls.auth,
 				type: 'GET',
@@ -30,17 +54,50 @@ $(function () {
 					success = false;
 					alertify.error(data[0].status + '<br />' + data[0].desc );
 				}
-				a.submit.callback(success, data[0]);
+				
+				if ( a.flags.saltRdy === true ) { // if salt request from change event is done and two second is passed
+					a.submit.callback(success, data[0], true);
+				} else if ( a.flags.saltRdy === false ) { // if salt request is not failed and two second is not passed
+					var flagChecker = setInterval(function () {
+						console.log(a.flags.saltRdy);
+						
+						if (a.flags.saltRdy === true) {
+							clearInterval(flagChecker);
+							a.submit.callback(success, data[0], true);
+							
+						} else if ( a.flags.saltReqSuccess === false ) {
+							clearInterval(flagChecker);
+							a.submit.callback(success, data[0], false);
+						}
+					}, a.constants.FLAG_CHECK_INTERVAL );
+				}
+			
+				/*	issue
+					if the user changes the username at the last minute, we set saltRdy to false,
+					so the form will submit and sees that saltRdy is false, and thinks a salt proccess is in action,
+					(weather the request isn't finished, or the request is finished but the salt waiting time isn't passed)
+					so we start checking (every 500ms) the two flags that represent the two states of a salt proccess,
+					inevitably one of those two flags will be set and we call the callback,
+					but the thing is,
+					because user didn't focusout of the input a new salt proccess is not going to be initiated,
+					so the salt proccess in action is for the previous value of the username input right before user changed it and clicked submit.
+				
+				
 			})
 			.fail(function () {
-				a.submit.callback(false);
+				a.submit.callback(false, undefined, false);
 			});
+			
+			*/
 		}
 	});
 	
 	$( a.dom.username ).on('change', function () {
 		//console.log(  $(this).val().trim()  );
-		if ( $(this).val().trim() == 0 ) { return; }
+		if ( $(this).val().trim().length === 0 ) { return; }
+		if ( a.flags.formSubmitted === true ) { return; }
+		
+		a.flags.saltRdy = false;
 		
 		$.ajax({
 			url: a.urls.auth,
@@ -53,16 +110,21 @@ $(function () {
 			beforeSend: a.change.beforeSend
 		})
 		.done(function (data) {
-			a.change.callback(true, data);
+			a.flags.saltReqSuccess = true;
+			a.data.salt = data;
+			
+			setTimeout(function () {
+				a.flags.saltRdy = true;
+			}, a.constants.SALT_WAIT_TIME );
+			
+			if ( a.flags.formSubmitted === false ) { a.change.callback(true, data); }
 		})
 		.fail(function () {
-			a.change.callback(false);
+			a.flags.saltReqSuccess = false;
+			if ( a.flags.formSubmitted === false ) { a.change.callback(false); }
 		});
 		
 	});
 	
 	$('.overlap').fadeOut();
-	
-	$( a.dom.shakeUser ).on( a.constants.ANI_END_EVENT, a.aniCssEnd);
-	$( a.dom.shakePass ).on( a.constants.ANI_END_EVENT, a.aniCssEnd);
 });
